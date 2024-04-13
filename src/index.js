95761b15aacfc10b4db97bff847de8af7661421f
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-04-13 23:39:30 +0200

    Change dom creation to use data from local storage

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import { projectList } from "./project-list";
+import { dataStorage } from "./data-storage";
+
 
 function screenController() {
   const projects = projectList()
 
   const addProjectButton = document.querySelector(".add-project-btn");
 
+  const randomNumber = () => {
+    return Math.floor(Math.random() * 20);
+  };
+
   const addProject = () => {
     projects.createNewProject(
-      "The Odin Project", 
+      `The Odin Project${randomNumber()}`, 
       "in Progress", 
       {
         projectDescription: "Learn Web Development", 
-        projectCategory: "Coding"
+        projectCategory: "Coding",
       }
     );
+
+    // projects.getListOfProjects().forEach((project, index) => {
+    //   dataStorage().postData(project, index);
+    // });
+    const index = projects.getListOfProjects().length - 1;
+    const newestProject = projects.getListOfProjects()[index];
+    dataStorage().postData(newestProject, index);
+
     updateNavbar();
   };
 
@@ -34,14 +48,20 @@ function screenController() {
     const projectSectionDOM = document.querySelector(".project-section");
 
     const projectsListDOM = document.createElement("ul");
-    projects.getListOfProjects().forEach(project => {
-      const listItem = document.createElement("li");
-      listItem.textContent = project.getProjectName();
-      projectsListDOM.appendChild(listItem);
-    });
-
+    dataStorage().getData().forEach(project => {
+        const listItem = document.createElement("li");
+        listItem.textContent = project.projectName;
+        projectsListDOM.appendChild(listItem);
+      });
     projectSectionDOM.appendChild(projectsListDOM);
   };
+
+  const siteLoad = () => {
+    if (dataStorage().getDataLength() === 0) return;
+    fillNavbarWithProjects();
+  };
+
+  siteLoad();
 
   addProjectButton.addEventListener("click", addProject);
 }
