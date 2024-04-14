35c6f0ee70a766e1f7381006864ed450b8b57b7c
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-04-14 20:59:09 +0200

    Change way projects are added to project list
    also remove unnecessary code

--- src/project-list.js
+++ src/project-list.js
@@ -2,64 +2,33 @@ import { Project } from "./project";
 import { dataStorage } from "./data-storage";
 
 function projectList() {
-  // const listOfProjects = [];
-  const listOfProjects = dataStorage().getData();
+  let savedProjects;
 
-  const getListOfProjects = () => listOfProjects;
+  if (dataStorage().getDataLength() === 0) {
+    savedProjects = [];
+  } else {
+    savedProjects = dataStorage().getData();
+  }
+  
+  const getSavedProjects = () => savedProjects;
 
-  // used for testing
-  const getProjectAttributes = () => {
-    listOfProjects.forEach(project => {
-      console.log(project.getProjectName());
-      console.log(project.getProjectState());
-    });
-  };
 
-  const createNewProject = (name, state, projectOptions) => {
-    const newProject = Project(name, state, projectOptions);
-    listOfProjects.push(newProject);
+  const listOfProjects = () => {
+    if (getSavedProjects().length === 0) return [];
+    return getSavedProjects().map(project => Project(project));
   };
 
   const deleteProject = (id) => {
-    const project = listOfProjects.filter(element => element.getProjectID() === id);
-    const index = listOfProjects.indexOf(project[0]);
-    if (index !== -1) listOfProjects.splice(index, 1);
+    const project = savedProjects.filter(element => element.getProjectID() === id);
+    const index = savedProjects.indexOf(project[0]);
+    if (index !== -1) savedProjects.splice(index, 1);
   }
 
   return {
-    getListOfProjects,
-    getProjectAttributes,
-    createNewProject,
+    getSavedProjects,
+    listOfProjects,
     deleteProject,
   };
 }
 
 export { projectList };
-
-
-
-
-// const projects = projectList();
-// projects.createNewProject("The Odin Project", "in Progress", {projectDescription: "Learn Web Development", projectCategory: "Coding"});
-// projects.createNewProject("Learn Blender", "halted", {projectCategory: "3D Art"});
-// projects.getProjectAttributes();
-// const projectID = projects.getListOfProjects()[1].getProjectID();
-// projects.getListOfProjects()[1].changeProjectName("Learn Japanese");
-// projects.deleteProject(projectID);
-// projects.getProjectAttributes();
-// projects.getListOfProjects()[1].changeProjectState("finished");
-// projects.getProjectAttributes();
-// const testProject = projects.getListOfProjects()[0];
-// const testProject2 = projects.getListOfProjects()[1];
-// testProject.createTodoItem("Learn Javascript", {priority: "high"});
-// testProject.createTodoItem("Learn CSS", {});
-// testProject.createTodoItem("Learn HTML", {});
-// const testID = testProject.printTodos();
-// testProject.deleteTodoItem(testID);
-// testProject.printTodos();
-// testProject2.createTodoItem("Watch Busstop Tutorial", {});
-// testProject2.createTodoItem("Watch Sword Tutorial", {});
-// testProject2.createTodoItem("Watch Random Tutorial", {});
-// const testID2 = testProject2.printTodos();
-// testProject2.deleteTodoItem(testID2);
-// testProject2.printTodos();
