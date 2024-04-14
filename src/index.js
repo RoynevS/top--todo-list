5a90752f917c12e51b518346475fc1ca03bab85d
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-04-14 21:00:33 +0200

    Change creation of new projects and DOM elements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { projectList } from "./project-list";
+import { createID } from "./project";
 import { dataStorage } from "./data-storage";
 
 
@@ -6,27 +7,26 @@ function screenController() {
   const projects = projectList()
 
   const addProjectButton = document.querySelector(".add-project-btn");
+  const addTodoButton = document.querySelector(".add-todo-btn");
 
   const randomNumber = () => {
     return Math.floor(Math.random() * 20);
   };
 
   const addProject = () => {
-    projects.createNewProject(
-      `The Odin Project${randomNumber()}`, 
-      "in Progress", 
-      {
+    const newProject = {
+      projectName: `The Odin Project${randomNumber()}`,
+      projectState: "in Progress",
+      projectID: createID(),
+      projectOptions: {
         projectDescription: "Learn Web Development", 
         projectCategory: "Coding",
-      }
-    );
+      },
+    };
 
-    // projects.getListOfProjects().forEach((project, index) => {
-    //   dataStorage().postData(project, index);
-    // });
-    const index = projects.getListOfProjects().length - 1;
-    const newestProject = projects.getListOfProjects()[index];
-    dataStorage().postData(newestProject, index);
+    projects.getSavedProjects().push(newProject);
+
+    dataStorage().postData(projects.getSavedProjects());
 
     updateNavbar();
   };
@@ -46,13 +46,21 @@ function screenController() {
 
   const fillNavbarWithProjects = () => {
     const projectSectionDOM = document.querySelector(".project-section");
-
     const projectsListDOM = document.createElement("ul");
-    dataStorage().getData().forEach(project => {
-        const listItem = document.createElement("li");
-        listItem.textContent = project.projectName;
-        projectsListDOM.appendChild(listItem);
+
+    projects.listOfProjects().forEach(project => {
+      const todoBtn = document.createElement("button");
+      todoBtn.classList.add("add-todo-btn");
+      todoBtn.textContent = "+ Todo";
+      todoBtn.dataset.id = project.getProjectID()
+      todoBtn.addEventListener("click", event => {
+        project.createTodoItem("This is just a tribute", {});
       });
+      const listItem = document.createElement("li");
+      listItem.textContent = project.getProjectName();
+      listItem.appendChild(todoBtn);
+      projectsListDOM.appendChild(listItem);
+    });
     projectSectionDOM.appendChild(projectsListDOM);
   };
 
@@ -64,6 +72,7 @@ function screenController() {
   siteLoad();
 
   addProjectButton.addEventListener("click", addProject);
+  // addTodoButton.addEventListener("click", addTodoButton);
 }
 
 screenController();
